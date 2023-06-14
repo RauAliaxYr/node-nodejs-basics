53d1fbeaedc7c6b6c0ec60dfd1756759a2b03dac
Aliaksandr <aliaksandr.rau@gmail.com>
2023-06-14 18:44:10 +0300

    feat: add copy file

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,12 @@
+import {cp} from 'node:fs/promises'
+
 const copy = async () => {
-    // Write your code here 
+    cp('files', 'foles_copy',
+        {
+            recursive: true,
+            errorOnExist:true,
+            force: false,
+        }).catch(err => console.error('FS operation failed'))
 };
 
 await copy();
