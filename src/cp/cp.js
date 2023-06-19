9ffdd281f3e5207e9a5672572d34302965a5f88c
Aliaksandr <aliaksandr.rau@gmail.com>
2023-06-19 20:52:13 +0300

    feat: add wt and cp tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,11 @@
+import {spawn} from 'node:child_process'
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = spawn('node', ['./files/script.js', ...args], { stdio: ['pipe', 'pipe', process.stderr] });
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
+
+    return childProcess;
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess("asdas","asdasd","asdas");
