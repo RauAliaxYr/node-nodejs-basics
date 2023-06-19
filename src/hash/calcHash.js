13030841ce48f5f2f5cd1d9aa2ff980928193d0b
Aliaksandr <aliaksandr.rau@gmail.com>
2023-06-19 04:03:36 +0300

    feat: add hash module

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,8 @@
+import {createHash} from "node:crypto"
+
 const calculateHash = async () => {
-    // Write your code here 
+    let hash = createHash('sha256').update('./files/fileToCalculateHashFor.txt').digest('hex')
+    console.log(hash)
 };
 
 await calculateHash();
