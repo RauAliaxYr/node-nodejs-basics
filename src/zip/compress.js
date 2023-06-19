1cf43d2bf6c0eaabbff7271994b0c26e9a726208
Aliaksandr <aliaksandr.rau@gmail.com>
2023-06-19 19:51:16 +0300

    feat: add zlib task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import * as fs from 'node:fs'
+import * as zlib from 'zlib'
+import {pipeline} from 'node:stream'
+
 const compress = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream('./files/fileToCompress.txt');
+    const writeStream = fs.createWriteStream('./files/archive.gz');
+    const gzipStream = zlib.createGzip();
+    pipeline(
+        readStream,
+        gzipStream,
+        writeStream,
+        (err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('Nice');
+            }
+        }
+    );
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import * as fs from 'node:fs'
+import * as zlib from 'zlib'
+import {pipeline} from 'node:stream'
+
 const decompress = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream('./files/archive.gz');
+    const writeStream = fs.createWriteStream('./files/fileToCompress.txt');
+    const gzipStream = zlib.createGunzip();
+    pipeline(
+        readStream,
+        gzipStream,
+        writeStream,
+        (err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('Nice');
+            }
+        }
+    );
 };
 
 await decompress();
